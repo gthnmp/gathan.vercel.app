215706d1efd6d5edfb4b2a27f082820c9dacb4b7
Gathan Mahesa <gthn217@gmail.com>
2023-06-23 11:02:29 +0700

    Added cross

---
+++ src/component/Cross.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import cross from './shaders/cross.svg'
+
+export default function Cross ({ crossVisible }){
+  return (
+    <>
+      <div className={`fixed top-1/2 left-52 w-16 h-16 z-50 transition-transform duration-500 ${crossVisible ? 'scale-100' : 'scale-0'}`}>
+        <img src = {cross} className=''></img>
+      </div>
+      <div className={`fixed top-1/2 right-52 w-16 h-16 z-50 transition-transform duration-500 ${crossVisible ? 'scale-100' : 'scale-0'}`}>
+        <img src = {cross} className=''></img>
+      </div>
+    </>
+  )
+}
